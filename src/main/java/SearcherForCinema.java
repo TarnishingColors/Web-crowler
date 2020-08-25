3de47f9ec497784031d9b7f248f7d29b4719bb56
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-08-25 16:36:24 +0300

    added func InitializeDoc and constructor to SearcherForCinema class

--- src/main/java/SearcherForCinema.java
+++ src/main/java/SearcherForCinema.java
@@ -1,10 +1,10 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 
 public class SearcherForCinema {
 
@@ -16,9 +16,41 @@ public class SearcherForCinema {
 
     private String city;
 
+    private String agent = "Chrome";
 
-    public SearcherForCinema(String siteAddress) {
+
+    public SearcherForCinema(String siteAddress,
+                             int date,
+                             String month,
+                             String city) {
+
         this.siteAddress = siteAddress;
+        this.date = date;
+        this.month = month;
+        this.agent = agent;
+
+    }
+
+
+    public Document InitializeDoc() {
+
+        boolean flag = true;
+        Document doc = new Document("");
+
+        while (flag) {
+
+            try {
+
+                doc = Jsoup.connect("http://afisha.ru")
+                        .userAgent("Mozilla").get();
+                flag = false;
+
+            } catch (Exception e) { continue; }
+
+        }
+
+        return doc;
+
     }
 
 
@@ -26,10 +58,17 @@ public class SearcherForCinema {
 
     public static void main(String[] args) throws IOException {
 
-        Document doc = Jsoup.connect("http://afisha.ru/msk")
-                .userAgent("Mozilla").get();
+        SearcherForCinema sfc = new SearcherForCinema(
+                "afisha.ru",
+                29,
+                "august",
+                "Moscow"
+                );
+
+        Document doc = sfc.InitializeDoc();
+
         System.out.print(doc.html());
-        Pattern pattern = Pattern.compile("(\\\\w*)cinema(\\\\w*)");
+        Pattern pattern = Pattern.compile("(\\w+\\.)*(\\w+)");
 
         for (Element e: doc.select("a[href]")) {
 
