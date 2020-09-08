68de9203d65d4aae4a5fc1364c8b7b03ac97ce33
Александр Габиташвили <aagabitashvili@edu.hse.ru>
2020-09-08 15:42:43 +0300

    created class SearcherForWeather

---
+++ src/main/java/org/searchapp/core/SearcherForWeather.java
@@ -0,0 +1,11 @@
+package org.searchapp.core;
+
+import org.jsoup.nodes.Document;
+
+public class SearcherForWeather implements Searcher {
+
+    @Override
+    public Document InitializeDoc() throws InterruptedException {
+        return null;
+    }
+}
